aec6b3bc73132333178a1765e8279e45e51afc4b
dute0987 <dute0987@gmail.com>
2022-03-16 21:31:52 +0530

    Vishal Dute- Main file for Give message for analyze

---
+++ MoodAnalyaze.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalayzer
+{
+    public class MoodAnalyse
+    {
+        public string AnalyseMood(string message)
+        {
+            if (message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
