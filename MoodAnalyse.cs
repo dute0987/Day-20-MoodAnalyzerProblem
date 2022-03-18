6b486847e8d86ef6d835e3bcef7bff8dbb8ed930
dute0987 <dute0987@gmail.com>
2022-03-18 18:27:02 +0530

    Vishal Dute - Main file for null Mood

---
+++ MoodAnalyse.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MoodAnalyse
+{
+    [TestClass]
+    public class AnalyzerTest
+    {
+        /// <summary>
+        /// 2.1: Given “I am in HAPPY Mood” and null message Should Return HAPPY
+        /// </summary>
+        [TestMethod]
+        [DataRow("I am in HAPPY Mood")]
+        [DataRow(null)]
+        public void GivenHAPPYMoodShouldReturnHappy(string message)
+        {
+            // Arrange
+            string expected = "HAPPY";
+            MoodAnalyse moodAnalyse = new MoodAnalyse(message);
+
+            // Act
+            string mood = moodAnalyse.AnalyseMood();
+
+            // Assert
+            Assert.AreEqual(expected, mood);
+        }
+    }
+}
