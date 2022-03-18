b049c6fd46ba2c2c69e57230220f0512b5e5636c
dute0987 <dute0987@gmail.com>
2022-03-18 18:12:03 +0530

    Vishal Dute - Main file for Sad Return Mood

---
+++ MoodAnalyse.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MoodAnalyse
+{
+    [TestClass]
+    public class AnalyzerTest
+    {
+        /// <summary>
+        /// TC 1.1: Given “I am in Sad Mood” message Should Return SAD.
+        /// </summary>
+        [TestMethod]
+        public static void GivenSadMoodShouldReturnSAD()
+        {
+            // Arrange
+            string expected = "SAD";
+            string message = "I am in Sad Mood";
+            MoodAnalyse moodAnalyse = new MoodAnalyse();
+
+            // Act
+            string mood = moodAnalyse.AnalyseMood(message);
+
+            // Assert
+            Assert.AreEqual(expected, mood);
+        }
+    }
+}
